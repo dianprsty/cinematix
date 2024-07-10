4eb2199e57761a76b57a9e61bf802e4d169f046f
dianprsty <dprasmail@gmail.com>
2024-07-10 15:15:20 +0700

    chore: move login logic to provider

--- lib/presentation/pages/login_page/login_page.dart
+++ lib/presentation/pages/login_page/login_page.dart
@@ -1,25 +1,24 @@
-import 'package:cinematix/data/dummies/dummy_authentication.dart';
-import 'package:cinematix/data/dummies/dummy_user_repository.dart';
+import 'package:cinematix/data/firebase/firebase_authentication.dart';
+import 'package:cinematix/data/firebase/firebase_user_repository.dart';
 import 'package:cinematix/domain/usecases/login/login.dart';
 import 'package:cinematix/presentation/pages/main_page/main_page.dart';
+import 'package:cinematix/presentation/providers/usecases/login_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends ConsumerWidget {
   const LoginPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(title: const Text("Login")),
       body: Center(
         child: ElevatedButton(
             onPressed: () {
-              Login login = Login(
-                authentication: DummyAuthentication(),
-                userRepository: DummyUserRepository(),
-              );
+              Login login = ref.watch(loginProvider);
 
-              login(LoginParams(email: "email", password: "password"))
+              login(LoginParams(email: "dian@mail.co", password: "123456"))
                   .then((result) {
                 if (result.isSuccess) {
                   Navigator.of(context).push(
