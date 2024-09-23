d2f5e88e2ad05b4c9f82240fbd5567298fad02a8
dianprsty <dprasmail@gmail.com>
2024-09-23 11:15:57 +0700

    add: seat widget

--- lib/presentation/pages/seat_booking_page/seat_booking_page.dart
+++ lib/presentation/pages/seat_booking_page/seat_booking_page.dart
@@ -1,5 +1,7 @@
 import 'package:cinematix/domain/entities/movie_detail.dart';
 import 'package:cinematix/domain/entities/transaction.dart';
+import 'package:cinematix/presentation/misc/method.dart';
+import 'package:cinematix/presentation/widgets/seat.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -17,7 +19,28 @@ class _SeatBookingPageState extends ConsumerState<SeatBookingPage> {
     final (moviedetail, transaction) = widget.transactionDetail;
     return Scaffold(
       body: Center(
-        child: Text(transaction.toString()),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Seat(
+              number: 1,
+              status: SeatStatus.reserved,
+              onTap: () {},
+            ),
+            horizontalSpace(16),
+            Seat(
+              number: 2,
+              status: SeatStatus.available,
+              onTap: () {},
+            ),
+            horizontalSpace(16),
+            Seat(
+              number: 3,
+              status: SeatStatus.selected,
+              onTap: () {},
+            ),
+          ],
+        ),
       ),
     );
   }
